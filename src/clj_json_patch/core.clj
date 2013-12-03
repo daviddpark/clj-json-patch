1e0d7ac3caf7b38d74f944366a6adb1edb4c273e
David Park <daviddpark@gmail.com>
2013-12-03 09:17:58 -0500

    added more support, test as op, and more tests

--- src/clj_json_patch/core.clj
+++ src/clj_json_patch/core.clj
@@ -1,27 +1,160 @@
 (ns clj-json-patch.core
   (:require [cheshire.core :as json])
-  (:use ;[clj-diff.core :only [clj-diff]]
-        [clojure.walk :only [walk]]))
+  (:use [clojure.walk :only [walk]]))
 
-(defn apply-op [obj op]
-  ;(println "(apply-op" op ")")
-  (cond (vector? obj)
-        (cond (= "move" (get op "op"))
-              (let [from (Integer/parseInt (second (re-find #"/(\d+)" (get op "from"))))
-                    to (Integer/parseInt (second (re-find #"/(\d+)" (get op "path"))))
-                    val (get obj from)]
-                (vec (concat (subvec obj 0 from) (subvec obj (inc from) (inc to))
-                             [(get obj from)] (subvec obj (inc to))))))))
+(declare remove-patch-value)
+
+(defn get-patch-value
+  "Given the patch path, find the associated value."
+  [obj path]
+  ;(println "(get-patch-value" obj path ")")
+  (if-let [match (re-find #"^/([^/]+)(.*)" path)]
+    (let [seg (second match)
+          segs (nth match 2)
+          val (cond (map? obj)
+                    (get obj seg)
+                    (vector? obj)
+                    (nth obj (Integer/parseInt seg)))]
+      (if-not (empty? segs)
+        (get-patch-value val segs)
+        val))))
+
+(defn set-patch-value
+  [obj path val]
+  (if-let [segs (re-seq #"/([^/]+)" path)]
+    (if (> (count segs) 1)
+      (let [parent-match (re-find #"(.*)(/[^/+])" path)
+            parent-path (apply str (map first (take (dec (count segs)) segs)))
+            parent (get-patch-value obj parent-path)]
+        (set-patch-value obj parent-path
+                         (set-patch-value parent (first (last segs)) val)))
+      (cond (map? obj)
+            (assoc obj (second (first segs)) val)
+            (vector? obj)
+            (let [idx (Integer/parseInt (second (re-find #"/(\d+)" path)))]
+              (try
+                (vec (concat (subvec obj 0 idx)
+                             [val]
+                             (subvec obj idx)))
+                (catch Exception e
+                  (throw (Exception. (str "Unable to set value at " idx))))))))
+    (throw (Exception. "Patch path must start with '/'"))))
+
+(defn move-patch-value
+  [obj from path]
+  ;(println "(move-patch-value" obj from path ")")
+  (if-let [to-segs (re-seq #"/([^/]+)" path)]
+    (if-let [from-segs (re-seq #"/([^/]+)" from)]
+      (if-let [val (get-patch-value obj from)]
+        (if (> (count to-segs) 1)
+          (let [from-parent-path (apply str (map first (take (dec (count from-segs)) from-segs)))
+                to-parent-path (apply str (map first (take (dec (count to-segs)) to-segs)))
+                parent (get-patch-value obj to-parent-path)]
+            (if (= from-parent-path to-parent-path)
+              (set-patch-value obj from-parent-path
+                               (move-patch-value (get-patch-value obj from-parent-path)
+                                                 (first (last from-segs))
+                                                 (first (last to-segs))))
+              (set-patch-value (remove-patch-value obj from)
+                               to-parent-path
+                               (set-patch-value parent (first (last to-segs)) val))))
+          (cond (map? obj)
+                (assoc obj (second (first to-segs)) val)
+                (vector? obj)
+                (let [from-int (try
+                                 (Integer/parseInt (second (re-find #"/(\d+)" from)))
+                                 (catch Exception e
+                                   (throw (Exception. (str "Move attempted on value that does not exist at '" from "'.")))))
+                        to-int (try
+                                 (Integer/parseInt (second (re-find #"/(\d+)" path)))
+                                 (catch Exception e
+                                   (throw (Exception. (str "Move attempted on value that does not exist at '" path "'.")))))]
+                    (vec (concat (subvec obj 0 from-int) (subvec obj (inc from-int) (inc to-int))
+                                 [(get obj from-int)] (subvec obj (inc to-int)))))
+                ))
+        (throw (Exception. (str "Move attempted on value that does not exist at '"
+                                from "'."))))
+      
+      (throw (Exception. "Patch 'from' value must start with '/'")))
+    (throw (Exception. "Patch 'path' value must start with '/'"))))
+
+(defn remove-patch-value
+  [obj path]
+  (try
+    (if-let [val (get-patch-value obj path)]
+      (if-let [segs (re-seq #"/([^/]+)" path)]
+        (if (> (count segs) 1)
+          (let [parent-path (apply str (map first (take (dec (count segs)) segs)))
+                parent (get-patch-value obj parent-path)]
+            (set-patch-value obj parent-path
+                             (remove-patch-value parent (first (last segs)))))
+          (cond (map? obj)
+                (dissoc obj (second (first segs)))
+                (vector? obj)
+                (let [idx (Integer/parseInt (second (re-find #"/(\d+)" path)))]
+                  (vec (concat (subvec obj 0 idx) (subvec obj (inc idx))))))))
+      (throw (Exception. (str "There is no value at '" path "' to remove."))))
+    (catch Exception e
+      (throw (Exception. (str "There is no value at '" path "' to remove."))))))
+
+(defn replace-patch-value
+  [obj path val]
+  (if-let [value (get-patch-value obj path)]
+    (if-let [segs (re-seq #"/([^/]+)" path)]
+      (if (> (count segs) 1)
+        (let [parent-path (apply str (map first (take (dec (count segs)) segs)))
+              parent (get-patch-value obj parent-path)]
+          (set-patch-value obj parent-path
+                           (replace-patch-value parent (first (last segs)) val)))
+        (cond (map? obj)
+              (assoc obj (second (first segs)) val)
+              (vector? obj)
+              (let [idx (Integer/parseInt (second (re-find #"/(\d+)" path)))]
+                (vec (concat (subvec obj 0 idx)
+                             [val]
+                             (subvec obj (inc idx)))))))
+      (throw (Exception. "Patch path must start with '/'")))
+    (throw (Exception. (str "Can't replace a value that does not exist at '" path "'.")))))
+
+(defn test-patch-value
+  [obj path val]
+  (try
+    (let [value (get-patch-value obj path)]
+      (if (not= val value)
+        (throw (Exception. (str "The test failed. '" val "' is not found at '" path "'.")))
+        obj))
+    (catch Exception e
+      (throw (Exception. (str "The test failed. '" val "' is not found at '" path "'."))))))
+
+(defn apply-patch [obj patch]
+  ;(println "")
+  ;(println "(apply-patch" patch ")")
+  (let [op (get patch "op")
+        path (get patch "path")
+        from (get patch "from")
+        value (get patch "value")]
+    (cond (= "add" op)
+          (set-patch-value obj path value)
+          (= "move" op)
+          (move-patch-value obj from path)
+          (= "remove" op)
+          (remove-patch-value obj path)
+          (= "replace" op)
+          (replace-patch-value obj path value)
+          (= "test" op)
+          (test-patch-value obj path value))))
 
 (defn patch
   "Applies a JSON patch document to JSON object"
   [obj patches]
-  (loop [patches patches
+  
+  (comment (loop [patches patches
          result obj]
     (if (empty? patches)
       result
       (recur (rest patches)
-             (apply-op result (first patches))))))
+             (apply-patch result (first patches))))))
+  (reduce #(apply-patch %1 %2) obj patches))
 
 (defn gen-op [t]
   [(let [result {"op" (first t) "path" (second t)}]
@@ -29,16 +162,27 @@
       (assoc result "value" (nth t 2))
       result))] )
 
+(defn clean-prefix
+  [prefix path]
+  (clojure.string/replace path (re-pattern prefix) "/"))
+
+(defn sanitize-prefix-in-patch
+  [prefix patch]
+  (let [path (get patch "path")
+        cleaned-path (assoc patch "path" (clean-prefix prefix path))]
+    (if-let [from (get patch "from")]
+      (assoc cleaned-path "from" (clean-prefix prefix from))
+      cleaned-path)))
+
 (defn diff-vecs [obj1 obj2 prefix]
   (loop [v1 obj1
          v2 obj2
          i 0
          ops []]
-    ;(println "diff-vecs" obj1 obj2 prefix)
-    ;(println "ops:" ops "patched:" (patch v1 ops))
-    ;(println v2 "=" (patch v1 ops) "?" (= v2 (patch v1 ops)))
+    ;(println "(diff-vecs" obj1 obj2 prefix ")")
+    ;(println "ops:" ops "patched:" (patch v1 (map (partial sanitize-prefix-in-patch prefix) ops)))
     (cond (and (> (count ops) 0)
-               (= v2 (patch v1 ops)))
+               (= v2 (patch v1 (map (partial sanitize-prefix-in-patch prefix) ops))))
           ops
           (= (set v1) (set v2))
           (do ;(println "count:" (count v1) "i:" i "ops:" ops)
@@ -78,21 +222,6 @@
            (if-let [idx (some identity (map-indexed #(if (= val %2) %1) obj))]
                (str prefix idx)
                (map-indexed #(get-value-path %2 val (str prefix %1 "/")) obj)))))
-
-(defn get-patch-value
-  "Given the patch path, find the associated value."
-  [obj path]
-  ;(println "(get-patch-value" obj path ")")
-  (if-let [match (re-find #"^/([^/]+)(.*)" path)]
-    (let [seg (second match)
-          segs (nth match 2)
-          val (cond (map? obj)
-                    (get obj seg)
-                    (vector? obj)
-                    (nth obj (Integer/parseInt seg)))]
-      (if-not (empty? segs)
-        (get-patch-value val segs)
-        val))))
 
 (defn transform-moves
   "Attempt to reconcile add/remove patch entries
--- test/clj_json_patch/core_test.clj
+++ test/clj_json_patch/core_test.clj
@@ -36,73 +36,6 @@
          (fact "get path from map of map with array"
                (get-value-path obj6 "deeper!") => "/foo/bar/1")))
 
-(facts "JSON diff"
-       (let [obj1 {"foo" "bar"}
-             obj2 {"foo" "bar"}
-             expected []]
-         (fact "No difference"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" "bar"}
-             obj2 {"baz" "qux" "foo" "bar"}
-             expected [{"op" "add" "path" "/baz" "value" "qux"}]]
-         (fact "Adding an Object Member"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" ["bar" "baz"]}
-             obj2 {"foo" ["qux" "bar" "baz"]}
-             expected [{"op" "add" "path" "/foo/0" "value" "qux"}]]
-         (fact "Adding an Array Element"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" ["bar" "baz"]}
-             obj2 {"foo" ["bar" "qux" "baz"]}
-             expected [{"op" "add" "path" "/foo/1" "value" "qux"}]]
-         (fact "Adding a second Array Element"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" "bar" "baz" "qux"}
-             obj2 {"foo" "bar"}
-             expected [{"op" "remove" "path" "/baz"}]]
-         (fact "Removing an Object Member"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" ["qux" "bar" "baz"]}
-             obj2 {"foo" ["bar" "baz"]}
-             expected [{"op" "remove" "path" "/foo/0"}]]
-         (fact "Removing first Array Element"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" ["bar" "qux" "baz"]}
-             obj2 {"foo" ["bar" "baz"]}
-             expected [{"op" "remove" "path" "/foo/1"}]]
-         (fact "Removing subsequent Array Element"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" ["bar" "baz" "qux"]}
-             obj2 {"foo" ["bar" "baz"]}
-             expected [{"op" "remove" "path" "/foo/2"}]]
-         (fact "Removing subsequent Array Element"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" "bar" "baz" "qux"}
-             obj2 {"baz" "boo" "foo" "bar"}
-             expected [{"op" "replace" "path" "/baz" "value" "boo"}]]
-         (fact "Replacing a Value"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" {"bar" "kill me"
-                          "baz" "boo"}}
-             obj2 {"foo" {"baz" "boo"}}
-             expected [{"op" "remove" "path" "/foo/bar"}]]
-         (fact "Adding a Nested Object Member"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" {"bar" "baz"
-                          "waldo" "fred"}
-                   "qux" {"corge" "grault"}}
-             obj2 {"foo" {"bar" "baz"}
-                   "qux" {"corge" "grault"
-                          "thud" "fred"}}
-             expected [{"op" "move" "from" "/foo/waldo" "path" "/qux/thud"}]]
-         (fact "Moving a Value"
-               (diff obj1 obj2) => expected))
-       (let [obj1 {"foo" ["all" "grass" "cows" "eat"]}
-             obj2 {"foo" ["all" "cows" "eat" "grass"]}
-             expected [{"op" "move" "from" "/foo/1" "path" "/foo/3"}]]
-         (fact "Moving an Array Element"
-               (diff obj1 obj2) => expected)))
-
 (facts "diff-vecs"
        (let [v1 ["all" "grass" "cows" "eat"]
              v2 ["all" "cows" "eat" "grass"]]
@@ -110,11 +43,11 @@
                (count (diff-vecs v1 v2 "/")) => 1
                (diff-vecs v1 v2 "/") => [{"from" "/1", "op" "move", "path" "/3"}])))
 
-(facts "apply-op"
+(facts "apply-patch"
        (let [v ["all" "grass" "cows" "eat" "slowly"]
              patch {"from" "/1", "op" "move", "path" "/3"}
              expected ["all" "cows" "eat" "grass" "slowly"]]
-         (apply-op v patch) => expected))
+         (apply-patch v patch) => expected))
 
 (facts "applying patch"
        (let [v ["all" "grass" "cows" "eat" "slowly"]
@@ -122,3 +55,185 @@
              expected ["all" "cows" "eat" "grass" "slowly"]]
          (patch v p) => expected))
 
+(facts "JSON diff"
+       (let [obj1 {"foo" "bar"}
+             obj2 {"foo" "bar"}
+             patches []]
+         (fact "No difference"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" "bar"}
+             obj2 {"baz" "qux" "foo" "bar"}
+             patches [{"op" "add" "path" "/baz" "value" "qux"}]]
+         (fact "Adding an Object Member"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" ["bar" "baz"]}
+             obj2 {"foo" ["qux" "bar" "baz"]}
+             patches [{"op" "add" "path" "/foo/0" "value" "qux"}]]
+         (fact "Adding an Array Element"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" ["bar" "baz"]}
+             obj2 {"foo" ["bar" "qux" "baz"]}
+             patches [{"op" "add" "path" "/foo/1" "value" "qux"}]]
+         (fact "Adding a second Array Element"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" "bar" "baz" "qux"}
+             obj2 {"foo" "bar"}
+             patches [{"op" "remove" "path" "/baz"}]]
+         (fact "Removing an Object Member"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" ["qux" "bar" "baz"]}
+             obj2 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/0"}]]
+         (fact "Removing first Array Element"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" ["bar" "qux" "baz"]}
+             obj2 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/1"}]]
+         (fact "Removing subsequent Array Element"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" ["bar" "baz" "qux"]}
+             obj2 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/2"}]]
+         (fact "Removing subsequent Array Element"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" "bar" "baz" "qux"}
+             obj2 {"baz" "boo" "foo" "bar"}
+             patches [{"op" "replace" "path" "/baz" "value" "boo"}]]
+         (fact "Replacing a Value"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" {"bar" "kill me"
+                          "baz" "boo"}}
+             obj2 {"foo" {"baz" "boo"}}
+             patches [{"op" "remove" "path" "/foo/bar"}]]
+         (fact "Adding a Nested Object Member"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" {"bar" "baz"
+                          "waldo" "fred"}
+                   "qux" {"corge" "grault"}}
+             obj2 {"foo" {"bar" "baz"}
+                   "qux" {"corge" "grault"
+                          "thud" "fred"}}
+             patches [{"op" "move" "from" "/foo/waldo" "path" "/qux/thud"}]]
+         (fact "Moving a Value"
+               (diff obj1 obj2) => patches))
+       (let [obj1 {"foo" ["all" "grass" "cows" "eat"]}
+             obj2 {"foo" ["all" "cows" "eat" "grass"]}
+             patches [{"op" "move" "from" "/foo/1" "path" "/foo/3"}]]
+         (fact "Moving an Array Element"
+               (diff obj1 obj2) => patches)))
+
+(facts "Happy path JSON patch"
+       (let [obj1 {"foo" "bar"}
+             patches []]
+         (fact "No change"
+               (patch obj1 patches) => obj1))
+       (let [obj1 {"foo" "bar"}
+             obj2 {"baz" "qux" "foo" "bar"}
+             patches [{"op" "add" "path" "/baz" "value" "qux"}]]
+         (fact "Adding an Object Member"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" ["bar" "baz"]}
+             obj2 {"foo" ["qux" "bar" "baz"]}
+             patches [{"op" "add" "path" "/foo/0" "value" "qux"}]]
+         (fact "Adding an Array Element"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" ["bar" "baz"]}
+             obj2 {"foo" ["bar" "qux" "baz"]}
+             patches [{"op" "add" "path" "/foo/1" "value" "qux"}]]
+         (fact "Adding a second Array Element"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" "bar" "baz" "qux"}
+             obj2 {"foo" "bar"}
+             patches [{"op" "remove" "path" "/baz"}]]
+         (fact "Removing an Object Member"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" ["qux" "bar" "baz"]}
+             obj2 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/0"}]]
+         (fact "Removing first Array Element"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" ["bar" "qux" "baz"]}
+             obj2 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/1"}]]
+         (fact "Removing subsequent Array Element"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" ["bar" "baz" "qux"]}
+             obj2 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/2"}]]
+         (fact "Removing subsequent Array Element"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" "bar" "baz" "qux"}
+             obj2 {"baz" "boo" "foo" "bar"}
+             patches [{"op" "replace" "path" "/baz" "value" "boo"}]]
+         (fact "Replacing a Value"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" {"bar" "kill me"
+                          "baz" "boo"}}
+             obj2 {"foo" {"baz" "boo"}}
+             patches [{"op" "remove" "path" "/foo/bar"}]]
+         (fact "Adding a Nested Object Member"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" {"bar" "baz"
+                          "waldo" "fred"}
+                   "qux" {"corge" "grault"}}
+             obj2 {"foo" {"bar" "baz"}
+                   "qux" {"corge" "grault"
+                          "thud" "fred"}}
+             patches [{"op" "move" "from" "/foo/waldo" "path" "/qux/thud"}]]
+         (fact "Moving a Value"
+               (patch obj1 patches) => obj2))
+       (let [obj1 {"foo" ["all" "grass" "cows" "eat"]}
+             obj2 {"foo" ["all" "cows" "eat" "grass"]}
+             patches [{"op" "move" "from" "/foo/1" "path" "/foo/3"}]]
+         (fact "Moving an Array Element"
+               (patch obj1 patches) => obj2))
+       (let [obj [ "a" 2 "c"]
+             patches [{ "op" "test" "path" "/1" "value" 2}]]
+         (fact "Successful test ops return object"
+               (patch obj patches) => obj))
+       (let [obj {"baz" "qux" "foo" [ "a" 2 "c"]}
+             patches [{ "op" "test" "path" "/baz" "value" "qux"}
+                      { "op" "test" "path" "/foo/1" "value" 2}]]
+         (fact "Successful test ops return object"
+               (patch obj patches) => obj)))
+
+(facts "Patch error conditions"
+       (let [obj1 {"foo" "bar"}
+             patches [{"op" "test" "path" "/baz" "value" "qux"}]]
+         (fact "Testing an Object Member"
+               (patch obj1 patches) => (throws Exception "The test failed. 'qux' is not found at '/baz'.")))
+       (let [obj1 ["foo" "bar"]
+             patches [{"op" "test" "path" "/4" "value" "qux"}]]
+         (fact "Testing an Array Member"
+               (patch obj1 patches) => (throws Exception "The test failed. 'qux' is not found at '/4'.")))
+       (let [obj1 {"foo" "bar"}
+             patches [{"op" "remove" "path" "/baz" "value" "qux"}]]
+         (fact "Removing an Object Member"
+               (patch obj1 patches) => (throws Exception "There is no value at '/baz' to remove.")))       
+       (let [obj1 {"foo" ["bar" "baz"]}
+             patches [{"op" "remove" "path" "/foo/6" "value" "qux"}]]
+         (fact "Removing an Array Element"
+               (patch obj1 patches) => (throws Exception "There is no value at '/foo/6' to remove.")))
+       (let [obj1 {"foo" ["bar" "baz"]}
+             patches [{"op" "add" "path" "/foo/3" "value" "qux"}]]
+         (fact "Adding a second Array Element"
+               (patch obj1 patches) => (throws Exception "Unable to set value at 3")))
+       (let [obj1 {"foo" "bar" "boo" "qux"}
+             patches [{"op" "replace" "path" "/baz" "value" "boo"}]]
+         (fact "Replacing a Value that doesn't exist"
+               (patch obj1 patches) => (throws Exception "Can't replace a value that does not exist at '/baz'.")))
+       (let [obj1 {"foo" {"beer" "kill me"
+                          "baz" "boo"}}
+             patches [{"op" "remove" "path" "/foo/bar"}]]
+         (fact "Removing a nested path that does not exist"
+               (patch obj1 patches) => (throws Exception "There is no value at '/foo/bar' to remove.")))
+       (let [obj1 {"foo" {"bar" "baz"
+                          "waldorf" "fred"}
+                   "qux" {"corge" "grault"}}
+             patches [{"op" "move" "from" "/foo/waldo" "path" "/qux/thud"}]]
+         (fact "Moving a Value that does not exist"
+               (patch obj1 patches) => (throws Exception "Move attempted on value that does not exist at '/foo/waldo'.")))
+       (let [obj1 {"foo" ["all" "grass" "cows" "eat"]}
+             patches [{"op" "move" "from" "/foo/1" "path" "/foo/-1"}]]
+         (fact "Moving an Array Element to an illegal value"
+               (patch obj1 patches) => (throws Exception "Move attempted on value that does not exist at '/-1'."))))
