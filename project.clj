4157c36eeb8917ded08f0a320e52b5e953af5381
David Park <daviddpark@gmail.com>
2019-01-15 14:51:53 -0500

    moderizing libraries

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-json-patch "0.1.5"
+(defproject clj-json-patch "0.1.6"
   :description "Clojure implementation of http://tools.ietf.org/html/rfc6902"
   :url "http://github.com/daviddpark/clj-json-patch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clj-json-patch.core
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.3.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.0.0"]]}})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.8.0"]]
+  :profiles {:dev {:dependencies [[midje/midje "1.9.5"]]
+                   :plugins [[lein-midje "3.2.1"]]}})
