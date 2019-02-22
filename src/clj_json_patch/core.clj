e76d5d0df327e2a8410f702d14bb4186d24f4598
Creighton Kirkendall <ckirkendall@gmail.com>
2019-02-22 16:34:30 -0500

    added support for keywords

--- src/clj_json_patch/core.clj
+++ src/clj_json_patch/core.clj
@@ -1,13 +1,20 @@
 (ns clj-json-patch.core
-  (:use [clj-json-patch.util :only [apply-patch diff*]]))
+  (:use [clj-json-patch.util :only [apply-patch diff* *keywordize*]]))
 
 
 (defn patch
   "Applies a JSON patch document (multiple patches) to JSON object."
-  [obj patches]
-  (reduce #(apply-patch %1 %2) obj patches))
+  ([obj patches]
+   (reduce #(apply-patch %1 %2) obj patches))
+  ([obj patches keywordize?]
+   (binding [*keywordize* keywordize?]
+     (reduce #(apply-patch %1 %2) obj patches))))
 
 (defn diff
   "Prepares a JSON patch document representing the difference
   between two JSON objects."
-  [obj1 obj2] (diff* obj1 obj2 "/"))
+  ([obj1 obj2]
+   (diff* obj1 obj2 "/"))
+  ([obj1 obj2 keywordize?]
+   (binding [*keywordize* keywordize?]
+     (diff* obj1 obj2 "/"))))
